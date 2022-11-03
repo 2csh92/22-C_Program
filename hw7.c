deb5ac630367dd18fd73e7115bb0d7a2a4187e8e
2csh92 <57474400+2csh92@users.noreply.github.com>
2022-11-03 13:20:48 +0900

    hw7.c 업로드

---
+++ hw7.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main() {
+	int arr1[6] = { 1,2,3,4,5,6 };
+	int arr2[6] = { 7,8,9,10,11,12 };
+	int* ptr1 = arr1;
+	int* ptr2 = arr2;
+	int t;
+	printf("arr1:");
+	for (int i = 0; i < 6; i++) {
+		printf(" %d", arr1[i]);
+	}
+	printf("\n");
+	printf("arr2:");
+	for (int i = 0; i < 6; i++) {
+		printf(" %d", arr2[i]);
+	}
+	for (int i = 0; i < 6; i++) {
+		t = *ptr1;
+		*ptr1 = *ptr2;
+		*ptr2 = t;
+		ptr1++;
+		ptr2++;
+	}
+	printf("\n\nafter swap\n");
+	printf("arr1:");
+	for (int i = 0; i < 6; i++) {
+		printf(" %d", arr1[i]);
+	}
+	printf("\n");
+	printf("arr2:");
+	for (int i = 0; i < 6; i++) {
+		printf(" %d", arr2[i]);
+	}
+}
